d4c8f1343b422667a8cc30916c6270e133773dae
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-13 16:43:27 +0000

    Thu, Oct 13, 2022, 4:43 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,26 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void printingHex(int row, int pyraHeight);
+void printingHex(int row);
 
 int main(void)
 {
+    bool state = true;
     int pyraHeight = 0;
     // checks user input to be 1-8
-    while (true)
+    while (state)
     {
     pyraHeight = get_int("Height: ");
     if ((pyraHeight >= 1) && (pyraHeight <= 8))
     {
-        break;
+        state = false;
     }
     }
-    printf("%*i\n", 3, pyraHeight);
+    // printf("%*i\n", 3, pyraHeight);
     // For each row
     for (int rowNum = 1; rowNum <= pyraHeight; rowNum++)
     {
         // for each whitespace in row on left hand side
         int numOfWhitespace = pyraHeight - rowNum;
+        for (int i = numOfWhitespace; i > 0; i--)
+        {
+            printf(" ");
+        }
+        // for each hex in row on left hand side
+        printingHex(rowNum);
+
+        printf(" ");
+        printf(" ");
+
+        // for each hex in row on right hand side
+        printingHex(rowNum);
 
         // next line
         printf("\n");
@@ -28,7 +41,7 @@ int main(void)
 }
 
 // print # based on row number
-void printingHex(int rowNum, int pyraHeight)
+void printingHex(int rowNum)
 {
     for (int k = rowNum; k > 0; k--)
     {
