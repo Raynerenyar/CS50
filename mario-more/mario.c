359569716d6c50365e9f27afff613af87075631b
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-13 14:56:46 +0000

    Thu, Oct 13, 2022, 2:56 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,38 +1,43 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void block(int height);
+// void block(int height);
 
 int main(void)
 {
     int getHeight = get_int("Height: ");
     // For row by row
-    for (int i = 0; i = getHeight; i++)
+    for (int i = 1; i = getHeight; i++)
     {
-        // for each unit in row
-        int numOfWhitespace = getHeight -i;
+        // for each whitespace in row
+        int numOfWhitespace = getHeight - i;
         for (int j = numOfWhitespace; j > 0; j--)
         {
             printf("\s");
         }
+        // for each # in row
+        for (int k = i; k = getHeight; k++)
+        {
+            printf("#");
+        }
     }
 }
 
-void block(int height)
-{
-    printf("test %i\n", height);
-    for (int i = 0; i < h; i++)
-    {
-        printf("#");
-    }
-    for (int h)
-    printf("\n");
-}
+// void block(int height)
+// {
+//     printf("test %i\n", height);
+//     for (int i = 0; i < h; i++)
+//     {
+//         printf("#");
+//     }
+//     for (int h)
+//     printf("\n");
+// }
 
-void printWhitespace(int row)
-{
-    for (int i = row; i > 0; i--)
-    {
+// void printWhitespace(int row)
+// {
+//     for (int i = row; i > 0; i--)
+//     {
 
-    }
-}
+//     }
+// }
