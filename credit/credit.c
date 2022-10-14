b80e50682bb053c78bf2787d4b636f842fde1bbc
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-14 10:24:28 +0000

    Fri, Oct 14, 2022, 10:24 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,6 +8,7 @@ int main(void)
     long num = get_long("Number: ");
     int count = countDigits(num);
     int numArray[count];
+    printf("%i\n",count);
     for (int i = count; i > 0; i--)
     {
         numArray[i] = num%10;
