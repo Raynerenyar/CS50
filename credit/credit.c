d04f7ef9df6004006b3912d0cbfabd2d3ceea953
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-14 10:01:32 +0000

    Fri, Oct 14, 2022, 10:01 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    long num = get_long("Number: ");
+
+    unsigned int count(unsigned int i) {
+    unsigned int ret=1;
+    while (i/=10) ret++;
+    return ret;
+}
 }
