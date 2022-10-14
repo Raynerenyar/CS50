60ee9e5e1d6ee3a0ab72043a2c1db27a508d216d
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-14 15:27:50 +0000

    Fri, Oct 14, 2022, 3:27 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,10 +7,10 @@ int main(void)
 {
     long num = get_long("Number: ");
     int count = countDigits(num);
-    int firstArrLen= (count/2);
-    int secArrLen = (count - (count/2));
+    int SecondArrLen= (count/2);
+    int firstArrLen = (count - (count/2));
+    int SecondArray[SecondArrLen];
     int firstArray[firstArrLen];
-    int secondArray[secArrLen];
     int i = count;
     int whichArray = 2;
     int k = 0;
@@ -19,60 +19,59 @@ int main(void)
     {
         if (whichArray ==  1)
         {
-            firstArray[k] = num%10;
+            SecondArray[k] = num%10;
             num /= 10;
             whichArray = 2;
-            printf("%i   %i",i, firstArray[k]);
+            printf("%i   %i",i, SecondArray[k]);
             k++;
         }
         else
         {
-            secondArray[l] = num%10;
+            firstArray[l] = num%10;
             num /= 10;
             whichArray = 1;
-            printf("%i   %i",i, secondArray[l]);
+            printf("%i   %i",i, firstArray[l]);
             l++;
         }
         i -= 1;
         printf("\n");
     }
     int sum = 0;
-    for (int a = 0; a < firstArrLen; a++)
+    for (int a = 0; a < SecondArrLen; a++)
     {
-        firstArray[a] *= 2;
-        if (firstArray[a] >= 10)
+        SecondArray[a] *= 2;
+        if (SecondArray[a] >= 10)
         {
-            sum += (1 + (firstArray[a]%10));
+            sum += (1 + (SecondArray[a]%10));
         }
-        else sum += firstArray[a];
+        else sum += SecondArray[a];
         printf("%i\n",sum);
     }
     int finalSum = 0;
-    for (int b = 0; b < secArrLen; b++)
+    for (int b = 0; b < firstArrLen; b++)
     {
-        if (secondArray[b] >= 10)
+        if (firstArray[b] >= 10)
         {
-            finalSum += (1 + (secondArray[b]%10));
+            finalSum += (1 + (firstArray[b]%10));
         }
-        else finalSum += secondArray[b];
+        else finalSum += firstArray[b];
     }
     printf("%i\n",(sum+finalSum));
 
     // checks if number is from AE, Mast, or Visa
-    if ()
 
     // check nums in first array
     // printf("first array\n");
-    // for (int m = 0; m < firstArrLen; m++)
+    // for (int m = 0; m < SecondArrLen; m++)
     // {
-    //     printf("%i %i\n", m,firstArray[m]);
+    //     printf("%i %i\n", m,SecondArray[m]);
     // }
     // printf("\n");
     // // check nums in second array
     // printf("second array\n");
-    // for (int n = 0; n < secArrLen; n++)
+    // for (int n = 0; n < firstArrLen; n++)
     // {
-    //     printf("%i %i\n", n, secondArray[n]);
+    //     printf("%i %i\n", n, firstArray[n]);
     // }
 }
 
