fab537e2532375259bc4be5a5c38172081a0af23
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-14 14:13:27 +0000

    Fri, Oct 14, 2022, 2:13 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,30 +7,32 @@ int main(void)
 {
     long num = get_long("Number: ");
     int count = countDigits(num);
-    int firstArray[(20)];
-    int secondArray[(20)];
+    int firstArrLen = count/2;
+    int secArrLen = count - count/2;
+    int firstArray[firstArrLen];
+    int secondArray[secArrLen];
     // int i = count;
     int whichArray = 2;
-    int firstArrInt = 1;
-    int secArrInt = 1;
     for (int i = count; i > 0; i--)
     {
         if (whichArray ==  1)
         {
-            firstArray[firstArrInt] = num%10;
+            firstArray[firstArrLen] = num%10;
             num /= 10;
             whichArray = 2;
-            printf("%i   %i",i, firstArray[i]);
+            printf("%i   %i",i, firstArray[firstArrLen]);
+            firstArrLen -= 1;
             // printf("%i",firstArray[i]);
             // printf("\n");
             // printf(" %li\n",num);
         }
         else
         {
-            secondArray[i] = num%10;
+            secondArray[secArrLen] = num%10;
             num /= 10;
             whichArray = 1;
-            printf("%i   %i",i, secondArray[i]);
+            printf("%i   %i",i, secondArray[secArrLen]);
+            secArrLen -= 1;
             // printf("%i",secondArray[i]);
             // printf("\n");
             // printf(" %li\n",num);
