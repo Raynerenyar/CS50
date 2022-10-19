935b4548ea9ae24131e03372b2e462e6a3a6c503
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-19 14:26:26 +0000

    Wed, Oct 19, 2022, 2:26 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -17,7 +17,9 @@ int main(void)
     int sentenceCount = countSentences(text, len);
     // organiseToArrays(text, wordCount);
     printf("WordCount: %i, letterCount: %i, sentenceCount: %i\n", wordCount, letterCount, sentenceCount);
-    double index = (0.0588 * (letterCount / wordCount * 100)) - (0.296 * (sentenceCount / wordCount * 100)) - 15.8;
+    double L = (letterCount / wordCount * 100);
+    double S = (sentenceCount / wordCount * 100);
+    double index = (0.0588 * L) - (0.296 * S) - 15.8;
     printf("%f\n", index);
     if (index > 16)
     {
