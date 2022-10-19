6b11a0763bac9d649c60abf662d7508bc54a3ca8
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-19 15:12:40 +0000

    Wed, Oct 19, 2022, 3:12 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,73 +1,75 @@
-#include <cs50.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+// #include <cs50.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
 
-int countWords(string text, int len);
-int count_letters(string text, int len);
-int countSentences(string text, int len);
+// int countWords(string text, int len);
+// int count_letters(string text, int len);
+// int countSentences(string text, int len);
 
-int main(void)
-{
-    string text = get_string("Text: ");
-    int len = strlen(text);
-    int wordCount = countWords(text, len);
-    int letterCount = count_letters(text, len);
-    int sentenceCount = countSentences(text, len);
-    printf("WordCount: %i, letterCount: %i, sentenceCount: %i\n", wordCount, letterCount, sentenceCount);
-    float L = ((float) letterCount / wordCount * 100);
-    float S = ((float) sentenceCount / wordCount * 100);
-    float index = (0.0588 * L) - (0.296 * S) - 15.8;
-    printf("%f\n", index);
-    if (index > 16)
-    {
-        printf("Grade 16+\n");
-    }
-    else if (index < 1)
-    {
-        printf("Before Grade 1\n");
-    }
-    else printf("Grade %i\n", (int) index);
-}
+// int main(void)
+// {
+//     string text = get_string("Text: ");
+//     int len = strlen(text);
+//     int wordCount = countWords(text, len);
+//     int letterCount = count_letters(text, len);
+//     int sentenceCount = countSentences(text, len);
+//     printf("WordCount: %i, letterCount: %i, sentenceCount: %i\n", wordCount, letterCount, sentenceCount);
+//     float L = ((float) letterCount / wordCount * 100);
+//     float S = ((float) sentenceCount / wordCount * 100);
+//     float index = (0.0588 * L) - (0.296 * S) - 15.8;
+//     printf("L is %f\n", L);
+//     printf("S is %f\n", S);
+//     printf("%f\n", index);
+//     if (index > 16)
+//     {
+//         printf("Grade 16+\n");
+//     }
+//     else if (index < 1)
+//     {
+//         printf("Before Grade 1\n");
+//     }
+//     else printf("Grade %i\n", (int) index);
+// }
 
-int countSentences(string text, int len)
-{
-    int sentenceCount = 0;
-    for (int i = 0; i < len; i++)
-    {
-        char character = text[i];
-        if (character == '!' || character == '.' || character == '?')
-        {
-            sentenceCount++;
-        }
-    }
-    return sentenceCount;
-}
+// int countSentences(string text, int len)
+// {
+//     int sentenceCount = 0;
+//     for (int i = 0; i < len; i++)
+//     {
+//         char character = text[i];
+//         if (character == '!' || character == '.' || character == '?')
+//         {
+//             sentenceCount++;
+//         }
+//     }
+//     return sentenceCount;
+// }
 
-int countWords(string text, int len)
-{
-    int wordCount = 0;
-    for (int i = 0; i < len; i++)
-    {
-        // char character = text[i];
-        if ((char) text[i] == ' ')
-        {
-            wordCount++;
-        }
-    }
-    return wordCount + 1;
-}
+// int countWords(string text, int len)
+// {
+//     int wordCount = 0;
+//     for (int i = 0; i < len; i++)
+//     {
+//         // char character = text[i];
+//         if ((char) text[i] == ' ')
+//         {
+//             wordCount++;
+//         }
+//     }
+//     return wordCount + 1;
+// }
 
-int count_letters(string text, int len)
-{
-    int letter_count = 0;
-    for (int i = 0; i < len; i++)
-    {
-        char character = text[i];
-        if (character != ' ' && character != '!' && character != '.' && character != '?' && character != ',')
-        {
-            letter_count++;
-        }
-    }
-    return letter_count;
-}
+// int count_letters(string text, int len)
+// {
+//     int letter_count = 0;
+//     for (int i = 0; i < len; i++)
+//     {
+//         char character = text[i];
+//         if (character != ' ' && character != '!' && character != '.' && character != '?' && character != ',')
+//         {
+//             letter_count++;
+//         }
+//     }
+//     return letter_count;
+// }
