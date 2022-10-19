0fbfa13918eb6a69ff4e23d4253ceca266a40152
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-19 12:28:28 +0000

    Wed, Oct 19, 2022, 12:28 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -6,13 +6,15 @@ string organiseToArrays(string text);
 int main(void)
 {
     string text = get_string("Text: ");
+    bool compare = text[0] == " ";
+    printf("%i\n", compare);
 }
 
-string organiseToArrays(string text)
-{
-    int len = strlen(text);
-    for (int i = 0; i < len; i++)
-    {
-        
-    }
-}
+// string organiseToArrays(string text)
+// {
+//     int len = strlen(text);
+//     for (int i = 0; i < len; i++)
+//     {
+//         if (text[i] == )
+//     }
+// }
