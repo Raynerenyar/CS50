f53f98a2c7a0fd936601e5266c400b3f2ce23ca2
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-19 12:33:00 +0000

    Wed, Oct 19, 2022, 12:33 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,21 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 string organiseToArrays(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
+    organiseToArrays(text);
 }
 
 string organiseToArrays(string text)
 {
     int len = strlen(text);
+    int word[];
     for (int i = 0; i < len; i++)
     {
-        if (text[i] == 32)
-        {
-            
-        }
+        word[i]  = text[i];
     }
 }
