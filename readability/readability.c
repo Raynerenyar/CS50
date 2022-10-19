f3cc0f9f50d92e1e274e8147ec63d0abaa29c3b9
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-19 15:29:00 +0000

    Wed, Oct 19, 2022, 3:29 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -26,7 +26,10 @@ int main(void)
     {
         printf("Before Grade 1\n");
     }
-    else printf("Grade %i\n", (int) round(index));
+    else
+    {
+        printf("Grade %i\n", (int) round(index));
+    }
 }
 
 int countSentences(string text, int len)
