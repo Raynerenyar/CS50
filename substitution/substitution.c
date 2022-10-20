e6948a45ec0eafd802cffaf61de2d5d4f97ea40d
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-20 12:35:35 +0000

    Thu, Oct 20, 2022, 12:35 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,27 @@
+#define _GNU_SOURCE
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    string key = argv[1];
+    int len = strlen(key);
+    bool correctLength = (len == 3); // demo with 3 first
+    // check num of args and length
+    if (argc == 2 && correctLength)
+    {
+        for (int i = 0; i < len; i++)
+        {
+            int onlyAlpha = isalpha(key[i]); // returns>0 if all alphabets
+            char letter = key[i];
+            key[i] = ' ';
+            char substring = strstr(key, letter);
+            if (substring)
+            {
+                printf("%c\n", key[i]);
+            }
+        }
+    }
 }
