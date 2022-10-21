cfa6ab8b1c654e586aa1d452014000fee25c4969
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-21 10:29:39 +0000

    Fri, Oct 21, 2022, 10:29 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -117,7 +117,7 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j=0; j < candidate_count - 1; j--)
+        for (int j=1; j < candidate_count; j++)
         {
             preferences[i][j]
         }
