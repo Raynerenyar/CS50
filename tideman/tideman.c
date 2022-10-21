f6be05131bcd83e7e535e58272becf68589847fe
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-21 15:58:17 +0000

    Fri, Oct 21, 2022, 3:58 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -155,9 +155,13 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    for (int i = 0; i < pair_count; i++)
+    int max = 0;
+    for (int i = pair_count; i >= 0; i--)
     {
-
+        if (preferences[pairs[i].winner][pairs[i].loser] > max)
+        {
+            max = preferences[pairs[i].winner][pairs[i].loser]
+        }
     }
     // TODO
     return;
