08f6cff526b0999ef3444c4202a379e89f8368be
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-21 13:59:28 +0000

    Fri, Oct 21, 2022, 1:59 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -120,9 +120,9 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    for (int i = 0; i < candidate_count - 1; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
-        for (int j=0; j < candidate_count-1; j++)
+        for (int j=1; j < candidate_count - 1; j++)
         {
             if (ranks[i] == ranks[j])
             {
