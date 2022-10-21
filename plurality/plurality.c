4217107860abdd4ebdf162cff0da955bd6465309
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-21 03:43:20 +0000

    Fri, Oct 21, 2022, 3:43 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,9 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    candidates
+
+    // return false if invalid
     return false;
 }
 
