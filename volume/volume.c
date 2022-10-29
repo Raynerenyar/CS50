d1e237ef1543d6f6d031f8f97d0d49c30a64785d
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-29 15:29:14 +0000

    Sat, Oct 29, 2022, 3:29 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -43,8 +43,8 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-    fread(&header, sizeof(header), 1, input);
-    fwrite(&header, sizeof(header), 1, output);
+    fread(&header, HEADER_SIZE, 1, input);
+    fwrite(&header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
     uint16_t buffer;
