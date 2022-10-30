44bfad3021dc377dde0d7c0add1049a7f3342554
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-30 13:45:35 +0000

    Sun, Oct 30, 2022, 1:45 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -49,7 +49,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         // reversing image
         for (int w = 0; w < width; w++)
         {
-            index = abs(w + width - 1);
+            index = abs(width - 1 - w);
             image[h][w] = row[h][index];
         }
     }
