f7f8b6fa633ad7e10a20c737f785f045d8cca66d
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-30 08:51:07 +0000

    Sun, Oct 30, 2022, 8:51 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -6,7 +6,6 @@
 
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
-typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -18,20 +17,12 @@ int main(int argc, char *argv[])
     }
 
     // Open files and determine scaling factor
-    // uint8_t *header = malloc(HEADER_SIZE + 1);
     FILE *input = fopen(argv[1], "r");
     if (input == NULL)
     {
         printf("Could not open file.\n");
         return 1;
     }
-    // else
-    // {
-    //     for (int i = 0; i < HEADER_SIZE + 1; i++)
-    //     {
-    //         header[i] = input[i];
-    //     }
-    // }
 
     FILE *output = fopen(argv[2], "w");
     if (output == NULL)
@@ -43,16 +34,16 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    BYTE header[HEADER_SIZE];
-    fread(header, sizeof(BYTE), 1, input);
-    fwrite(header, sizeof(BYTE), 1, output);
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    while (fread(&buffer, sizeof(uint16_t), 1, input))
+    while (fread(&buffer, sizeof(int16_t), 1, input))
     {
-        buffer = buffer * factor;
-        fwrite(&buffer, sizeof(uint16_t), 1,  output);
+        buffer *= factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
     }
 
     // Close files
