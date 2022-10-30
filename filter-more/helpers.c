ad9d64c135bc178d85723ac482fe4dda28bb0a87
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-30 11:43:52 +0000

    Sun, Oct 30, 2022, 11:43 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -33,10 +33,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             // BYTE red = image[h][w].rgbtRed;
             row[w] = image[h][w];
         }
-        int index;
+        uint index;
         for (int r = width; r > 0; r--)
         {
-            int index = r - index;
+            uint index = r - index;
             image[h][] = row[r];
         }
 
