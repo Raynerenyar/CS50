b9e7a01664453b4d6d402cb2e2b49416def34ce5
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-31 16:01:58 +0000

    Mon, Oct 31, 2022, 4:01 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc > 2)
+    {
+        printf("Only 1 command-line argument is allowed\n");
+        return 1;
+    }
+    
 }
