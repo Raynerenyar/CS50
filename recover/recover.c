71534ff4c1a8215b1f645bf2b89b3dec277f59e6
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-10-31 16:19:44 +0000

    Mon, Oct 31, 2022, 4:19 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,5 +8,15 @@ int main(int argc, char *argv[])
         printf("Only 1 command-line argument is allowed\n");
         return 1;
     }
-    
+    // open memory card
+    // repeat until end of card:
+        // read 512 bytes into buffer
+        // if start of a new jpeg
+            // if first jpeg
+                // write to new file with ###.jpg
+            // else
+                // close file
+        // else
+            // if already found jpeg
+
 }
