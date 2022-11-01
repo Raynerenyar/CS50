74e3a07cd8226e2b4e3d1848ac3e87c6001b8c1d
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-01 08:39:32 +0000

    Tue, Nov 1, 2022, 8:39 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -54,21 +54,25 @@ int check_signature(BYTE signature[4])
 {
     for (int j = 0; j < 4; j++)
     {
-        if (signature[i] == 0xff)
+        if (signature[j] == 0xff)
         {
             continue;
         }
-        if (signature[i] == 0xd8)
+        if (signature[j] == 0xd8)
         {
             continue;
         }
-        if (signature[i] == 0xff)
+        if (signature[j] == 0xff)
         {
             continue;
         }
-        if ( (signature[i] & 0xf0) == 0xe0 )
+        if ( (signature[j] & 0xf0) == 0xe0 )
         {
             return 1;
+        }
+        else
+        {
+            return 0;
         }
 
     }
