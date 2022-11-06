b858201720074e7d4652d018d83f86919df40f0d
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-06 11:27:43 +0000

    Sun, Nov 6, 2022, 11:27 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -40,13 +40,18 @@ unsigned int hash(const char *word)
 // int prime[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
 
 //
-node create_word(char word, int beginning_of_word)
+node create_word(char word, int letter_count)
 {
     node *w = malloc(sizeof(node));
+    w->word[letter_count - 1];
 
-    if (beginning_of_word > 1)
+    if (letter_count > 1)
     {
-
+        create_word(word, letter_count - 1);
+    }
+    else
+    {
+        w->next = NULL;
     }
 }
 
