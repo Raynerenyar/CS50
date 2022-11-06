c95143daed0811d9c8292fc744d2ada1e9d0e561
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-06 02:17:08 +0000

    Sun, Nov 6, 2022, 2:17 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-// #include <cs50.h>
-
+// def datatype
 typedef uint8_t BYTE;
+// def function
 int check_signature(BYTE signature[4]);
 
 int main(int argc, char *argv[])
@@ -16,24 +16,27 @@ int main(int argc, char *argv[])
     }
     if (argc < 2)
     {
-        printf("Usage: ./recover card.raw\n");
+        printf("Usage: ./recover IMAGE\n");
         return 1;
     }
-    char *forensicImage = argv[1];
 
     // open memory card
+    char *forensicImage = argv[1];
     FILE *raw = fopen(forensicImage, "r");
     int reading = 1;
     // jpeg image header
     BYTE signature[4];
     BYTE jpeg_data[512 - 4];
-    // repeat until end of card:
+
+    // image file counter
     int counter = 0;
+
+    // read first 4 bytes of buffer for jpeg signature
     reading = fread(signature, 4, 1, raw);
+
+    // in each loop, reads 4 + 508 bytes of each block
     while (reading > 0)
     {
-        // read 1 block of buffer
-        // read first 4 bytes of buffer for jpeg signature
         if (check_signature(signature) == 0) // 0 if jpeg signature found
         {
             // create image
@@ -44,14 +47,16 @@ int main(int argc, char *argv[])
             fread(&jpeg_data, 512 - 4, 1, raw);
             fwrite(jpeg_data, 512 - 4, 1, outptr);
             int same_image = 1;
+
+            // loops if next block does not contain jpeg signature
             while (same_image == 1)
             {
                 reading = fread(signature, 4, 1, raw);
-                if (check_signature(signature) != 0)
+                if (check_signature(signature) != 0 && reading > 0)
                 {
-                fwrite(signature, 4, 1, outptr);
-                fread(&jpeg_data, 512 - 4, 1, raw);
-                fwrite(jpeg_data, 512 - 4, 1, outptr);
+                    fwrite(signature, 4, 1, outptr);
+                    fread(&jpeg_data, 512 - 4, 1, raw);
+                    fwrite(jpeg_data, 512 - 4, 1, outptr);
                 }
                 else
                 {
@@ -61,28 +66,24 @@ int main(int argc, char *argv[])
                 }
             }
         }
+        // jpeg signature not found, read rest of the 508 bytes
         else
         {
             // read rest of block
             fread(&jpeg_data, 512 - 4, 1, raw);
+
+            // if reading < 0, breaks while loop
             reading = fread(signature, 4, 1, raw);
         }
     }
-        // if start of a new jpeg
-            // if first jpeg
-                // write to new file with ###.jpg
-            // else
-                // close file
-        // else
-            // if already found jpeg
-                // ...
-    // close any remaining files
     fclose(raw);
+    return 0;
 }
 
+// check if signature exists
 int check_signature(BYTE signature[4])
 {
-    if ( signature[0] == 0xff && signature[1] == 0xd8 && signature[2] == 0xff && (signature[3] & 0xf0) == 0xe0 )
+    if (signature[0] == 0xff && signature[1] == 0xd8 && signature[2] == 0xff && (signature[3] & 0xf0) == 0xe0)
     {
         return 0;
     }
