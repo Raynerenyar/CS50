0215d98843570ad4c0385874840e4c26da2286cb
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-06 02:44:18 +0000

    Sun, Nov 6, 2022, 2:44 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -34,6 +34,7 @@ unsigned int hash(const char *word)
 }
 
 // Loads dictionary into memory, returning true if successful, else false
+// hash table of some kind
 bool load(const char *dictionary)
 {
     // TODO
