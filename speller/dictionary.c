31cdb68bc28dcf7b090909fc06b5286eb1737d17
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-06 08:13:46 +0000

    Sun, Nov 6, 2022, 8:13 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -61,53 +61,54 @@ bool load(const char *dictionary)
     FILE *txt_dict = fopen(filename, "r");
     if (txt_dict != NULL)
     {
-        // count num of words
-        int word_count = 0;
-        char letter = fread(&letter, sizeof(char), 1, txt_dict);
-        while (letter != 0)
-        {
-            if (letter != 10)
-            {
-                word_count++;
-            }
-            letter = fread(&letter, sizeof(char), 1, txt_dict);
-        }
-        fclose(txt_dict);
-        hashedWord dictionary_of_words[word_count];
-        // *dictionary_of_words = malloc(word_count * sizeof(hashedWord));
-        // reopen txt file
-        FILE *dict = fopen(filename, "r");
-        letter = fread(&letter, sizeof(char), 1, dict);
-        char word[LENGTH + 1];
-        int i = 0;
-        while (letter != 0) // != null
-        {
-            while (letter != 10) // != \n
-            {
-                // append to string;
-                word[i] = letter;
-                i++;
-            }
-            int len_of_word = LENGTH + 1;
-            int hash = 0;
-            hashedWord *hashed_word = malloc(sizeof(int) + sizeof(word[LENGTH + 1]));
-            for (int j = 0; j < len_of_word; j++)
-            {
-                if (word[j] != 0)
-                {
-                    // polynomial hashing
-                    hash += word[j] * prime[j];
-                    hashed_word->word[j] = word[j];
-                }
-                else
-                {
-                    break;
-                }
-            }
-            hashed_word->index = hash;
-            // read next char
-            letter = fread(&letter, sizeof(char), 1, dict);
-        }
+        
+        // // count num of words
+        // int word_count = 0;
+        // char letter = fread(&letter, sizeof(char), 1, txt_dict);
+        // while (letter != 0)
+        // {
+        //     if (letter != 10)
+        //     {
+        //         word_count++;
+        //     }
+        //     letter = fread(&letter, sizeof(char), 1, txt_dict);
+        // }
+        // fclose(txt_dict);
+        // hashedWord dictionary_of_words[word_count];
+        // // *dictionary_of_words = malloc(word_count * sizeof(hashedWord));
+        // // reopen txt file
+        // FILE *dict = fopen(filename, "r");
+        // letter = fread(&letter, sizeof(char), 1, dict);
+        // char word[LENGTH + 1];
+        // int i = 0;
+        // while (letter != 0) // != null
+        // {
+        //     while (letter != 10) // != \n
+        //     {
+        //         // append to string;
+        //         word[i] = letter;
+        //         i++;
+        //     }
+        //     int len_of_word = LENGTH + 1;
+        //     int hash = 0;
+        //     hashedWord *hashed_word = malloc(sizeof(int) + sizeof(word[LENGTH + 1]));
+        //     for (int j = 0; j < len_of_word; j++)
+        //     {
+        //         if (word[j] != 0)
+        //         {
+        //             // polynomial hashing
+        //             hash += word[j] * prime[j];
+        //             hashed_word->word[j] = word[j];
+        //         }
+        //         else
+        //         {
+        //             break;
+        //         }
+        //     }
+        //     hashed_word->index = hash;
+        //     // read next char
+        //     letter = fread(&letter, sizeof(char), 1, dict);
+        // }
     }
     return false;
 }
