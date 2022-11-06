0cb3d5efe7889ca4099874c34eda770d45923aa5
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-06 13:08:48 +0000

    Sun, Nov 6, 2022, 1:08 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -41,9 +41,11 @@ node *create_word(char one_word[], int letter_count, int start)
 {
     node *w = malloc(sizeof(node));
     int index = one_word[letter_count - 1] - 97;
+    if (start ==0)
+    
     if (letter_count > 1)
     {
-        node *next_word = create_word(one_word, letter_count - 1);
+        node *next_word = create_word(one_word, letter_count - 1, start);
         w->word[index] = one_word[start];
         w->next = next_word;
     }
