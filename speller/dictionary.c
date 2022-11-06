9679859c351c9ede8b01103d5f17293175800cfb
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-06 03:41:24 +0000

    Sun, Nov 6, 2022, 3:41 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -38,6 +38,11 @@ unsigned int hash(const char *word)
 int prime[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
 
 //
+typedef struct dict
+{
+    int index;
+    char word
+}
 
 // Loads dictionary into memory, returning true if successful, else false
 // hash table of some kind
