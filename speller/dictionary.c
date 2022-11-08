734668960e1bb06b0b6fc05f97c8068b1df6ae58
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-08 17:44:29 +0000

    Tue, Nov 8, 2022, 5:44 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -18,7 +18,7 @@ typedef struct node
 node;
 
 // 200 number of buckets in hash table
-const unsigned int N = 100000;
+const unsigned int N = 200000;
 
 // tracks dictionary size
 int dict_size = 0;
