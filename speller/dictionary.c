bf7bd5130ce31b455487010f62e525e19d2ed1fa
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-08 17:10:56 +0000

    Tue, Nov 8, 2022, 5:10 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -95,6 +95,7 @@ bool load(const char *dictionary)
             w->next = table[hash_value]; // point node to head of bucket
             table[hash_value] = w; // insert new node
             fread(&letter, sizeof(char), 1, txt_dict);
+            memset ( one_word, 0, N*sizeof(char) ) ;
             letter_count = 0;
             dict_size++;
 
