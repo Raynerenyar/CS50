863ca3da768809a9ee5645de82af65ff2a312b38
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-28 16:10:25 +0000

    Mon, Nov 28, 2022, 4:10 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -7,5 +7,4 @@ digitArr = [int(digit) for digit in str(digits)]
 len = len(digitArr)
 print("length of card num is " + str(len))
 for i in range(len-1,0,-2):
-    
-    print(i)
+    print(digitArr[i - 1])
