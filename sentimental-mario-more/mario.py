87cefdaf35865cd72841ce1ba5e3090cae1c378c
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-28 15:35:14 +0000

    Mon, Nov 28, 2022, 3:35 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,14 +2,17 @@ from cs50 import get_int
 
 while (True):
     getHeight = get_int("Height: ")
-    if not (getHeight >= 1 and getHeight <=8):
+    if not (getHeight >= 1 and getHeight <= 8):
         continue
     else:
         break
 
 whitespace = 0
-for i in range(1,getHeight+1):
+for i in range(1, getHeight+1):
     whitespace = getHeight - i
-    print((" " * whitespace)+("#" * i), end = '')
-    print("  ", end = '')
+    # left side of pyramid
+    print((" " * whitespace)+("#" * i), end='')
+    # middle 2 whitespaces
+    print("  ", end='')
+    # right side of pyramid
     print("#" * i)
