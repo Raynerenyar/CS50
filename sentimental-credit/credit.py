74498ee5661fe4d4961dc1907101937e335a88ed
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-28 16:35:05 +0000

    Mon, Nov 28, 2022, 4:35 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,21 +3,24 @@ from cs50 import get_int
 digits = get_int("Number: ")
 
 # checksum
-digitArr = [int(digit) for digit in str(digits)]
-len = len(digitArr)
-print("length of card num is " + str(len))
-sumOfProducts = 0
-for i in range(len-1,0,-2):
-    digit = digitArr[i - 1] * 2
-    # print(i)
-    if (digit >= 10): # max digit = 9. max product is 19. Therefore first digit is always 1 if product >= 10
-        sumOfProducts += (1 + digit % 10)
-    else:
-        sumOfProducts += digit
-# print(sumOfProducts)
-sumOfNonProducts = 0
-for i in range(len,0,-2):
-    sumOfNonProducts += digitArr[i - 1]
-sumOfSums = sumOfNonProducts + sumOfProducts
 
+def checksum():
+    digitArr = [int(digit) for digit in str(digits)]
+    length = len(digitArr)
+    print("length of card num is " + str(length))
+    sumOfProducts = 0
+    for i in range(len-1,0,-2):
+        digit = digitArr[i - 1] * 2
+        # print(i)
+        if (digit >= 10): # max digit = 9. max product is 19. Therefore first digit is always 1 if product >= 10
+            sumOfProducts += (1 + digit % 10)
+        else:
+            sumOfProducts += digit
+    # print(sumOfProducts)
+    sumOfNonProducts = 0
+    for i in range(len,0,-2):
+        sumOfNonProducts += digitArr[i - 1]
+    sumOfSums = sumOfNonProducts + sumOfProducts
+    return (sumOfSums % 10) == 0
 
+print(checksum())
