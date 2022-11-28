909d80d1c3ef66aa097da0c3e5cc4b3fef7fd4dc
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-28 15:09:30 +0000

    Mon, Nov 28, 2022, 3:09 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,11 +1,14 @@
 from cs50 import get_int
 
-while (True):
-    getHeight = get_int("Height: ")
-    if (not (getHeight >= 1) and not (getHeight <=8)):
-        continue
-    else:
-        break
+# while (True):
+#     getHeight = get_int("Height: ")
+#     if (not getHeight >= 1 and not getHeight <=8):
+#         continue
+#     else:
+#         break
+
+getHeight = get_int("Height: ")
+print(not getHeight >= 1 and not getHeight<= 8)
 
 for i in range(getHeight):
     print("#")
