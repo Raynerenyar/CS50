3fb0cacd1e8dab3cdc3eb0ebc9839ab353903e79
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-28 16:21:55 +0000

    Mon, Nov 28, 2022, 4:21 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -9,9 +9,10 @@ print("length of card num is " + str(len))
 sumOfProducts = 0
 sumofNonProducts = 0
 for i in range(len-1,0,-2):
-    if (digitArr[i - 1] >= 10): # max digit = 9. max product is 19. Therefore first digit is always 1 if product >= 10
-        sumOfProducts += (1 + digitArr[i - 1] % 10)
+    digit = digitArr[i - 1] * 2
+    if (digit >= 10): # max digit = 9. max product is 19. Therefore first digit is always 1 if product >= 10
+        sumOfProducts += (1 + digit % 10)
     else:
-        sumOfProducts += digitArr[i - 1] * 2
+        sumOfProducts += digit * 2
 print(sumOfProducts)
 # for i in range(len,0,-2):
