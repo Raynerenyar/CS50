0ee12e86c156c7b59a14b68a4e1d35022dce9555
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-11-28 16:08:36 +0000

    Mon, Nov 28, 2022, 4:08 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,5 +5,7 @@ digits = get_int("Number: ")
 # checksum
 digitArr = [int(digit) for digit in str(digits)]
 len = len(digitArr)
-for i in range(len,0,1):
+print("length of card num is " + str(len))
+for i in range(len-1,0,-2):
+    
     print(i)
