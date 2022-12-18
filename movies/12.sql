82f25540b35f044ca9ffa5309c7cacf1cf933c49
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-18 18:07:22 +0000

    Sun, Dec 18, 2022, 6:07 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,5 +2,8 @@ SELECT title from movies
 INNER JOIN stars ON stars.movie_id = movies.id
 INNER JOIN people ON people.id = stars.person_id
 WHERE people.name = "Johnny Depp"
-AND
-people.name = "Helena Bonham Carter";
+AND title IN
+SELECT title from movies
+INNER JOIN stars ON stars.movie_id = movies.id
+INNER JOIN people ON people.id = stars.person_id
+WHERE people.name = "Helena Bonham Carter";
