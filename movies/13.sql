ed2ad87e0d52483b56dd2a8056b987f581177a1b
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-18 18:52:00 +0000

    Sun, Dec 18, 2022, 6:52 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,7 +1,11 @@
 SELECT name FROM people
 INNER JOIN stars ON stars.person_id = people.id
 INNER JOIN movies ON movies.id = stars.movie_id
+
+-- filters
 WHERE movies.id IN
+
+-- returns table of id from movies where the 1958 Kevin Bacon starred in
 (SELECT movies.id from movies
 INNER JOIN stars ON stars.movie_id = movies.id
 INNER JOIN people ON people.id = stars.person_id
