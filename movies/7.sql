0e800faf7006951ec3e3caeb703ffb49cc1cf6e5
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-18 17:24:40 +0000

    Sun, Dec 18, 2022, 5:24 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,2 +1,6 @@
 SELECT rating FROM ratings WHERE movie_id IN (SELECT id FROM movies WHERE year = 2010);
 SELECT title,id FROM movies WHERE year = 2010;
+
+SELECT ratings.rating, movies.title
+from movies
+inner join ratings on movies.id = ratings.movie_id
