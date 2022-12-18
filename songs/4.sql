091a19df8a3d8c8b73ff467f517f5222c5e1f31a
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-18 15:19:03 +0000

    Sun, Dec 18, 2022, 3:19 PM +00:00

--- songs/4.sql
+++ songs/4.sql
@@ -1,4 +1,4 @@
 SELECT name from songs WHERE danceability > 0.75;
 SELECT name from songs WHERE energy > 0.75;
 SELECT name from songs WHERE valence > 0.75;
-SELECT name from songs WHERE valence IN (SELECT valence FROM songs WHERE energy > 0.75);
+SELECT name from songs WHERE valence > 0.75 IN (SELECT valence FROM songs WHERE energy > 0.75);
