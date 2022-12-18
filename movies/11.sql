664277f855950b2a794f841ee1241ca90ddef64a
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-18 18:02:15 +0000

    Sun, Dec 18, 2022, 6:02 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,3 +1,7 @@
 SELECT title FROM movies
 INNER JOIN stars ON stars.movie_id = movies.id
-INNER JOIN ratings on 
+INNER JOIN ratings ON ratings.movie_id = movies.id
+INNER JOIN people ON people.id = stars.person_id
+WHERE people.name = "Chadwick Boseman"
+ORDER BY ratings.rating DESC
+LIMIT 5;
