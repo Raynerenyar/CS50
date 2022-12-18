576086e339197f526105c156a1f48982c7515399
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-18 18:26:55 +0000

    Sun, Dec 18, 2022, 6:26 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,9 @@
-SELECT people.name FROM people
+-- SELECT people.name FROM people
+-- INNER JOIN stars ON stars.movie_id = movies.id
+-- INNER JOIN people ON people.id = stars.person_id
+-- WHERE people.name = "Kevin Bacon";
+
+SELECT title from movies
 INNER JOIN stars ON stars.movie_id = movies.id
 INNER JOIN people ON people.id = stars.person_id
 WHERE people.name = "Kevin Bacon";
