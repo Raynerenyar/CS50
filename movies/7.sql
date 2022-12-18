57f04417b2f54ebd61b35357415614e78c9b21b9
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-18 17:01:28 +0000

    Sun, Dec 18, 2022, 5:01 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,2 @@
-SELECT title,rating FROM ratings WHERE movie_id IN (SELECT id,title FROM movies WHERE year = 2010);
+SELECT rating FROM ratings WHERE movie_id IN (SELECT id FROM movies WHERE year = 2010);
+SELECT title FROM movies WHERE year = 2019;
