841c9368efccd59e34e15cf75031627babb342d7
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-19 08:05:18 +0000

    Mon, Dec 19, 2022, 8:05 AM +00:00

--- fiftyville/small.sql
+++ fiftyville/small.sql
@@ -1,8 +1,10 @@
 select * from people
     inner join bank_accounts on bank_accounts.person_id = people.id
     inner join atm_transactions on bank_accounts.account_number = atm_transactions.account_number
-    where year = 2021
-    and month = 7
-    and day = 28
+    inner join bakery_security_logs on bakery_security_logs.license_plate = people.license_plate
+    where atm_transactions.year = 2021
+    and atm_transactions.month = 7
+    and atm_transactions.day = 28
+    and atm_transactions.hour = 10
     and atm_transactions.transaction_type = "withdraw"
     and atm_transactions.atm_location = "Leggett Street";
