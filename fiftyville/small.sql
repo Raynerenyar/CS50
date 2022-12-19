be3d99cfa284547c908934f218d581ad88407287
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-19 06:35:22 +0000

    Mon, Dec 19, 2022, 6:35 AM +00:00

--- fiftyville/small.sql
+++ fiftyville/small.sql
@@ -1,6 +1,3 @@
-select *
-    from atm_transactions
-    where day = 28
-    and month = 7
-    and year = 2021
-    and atm_location = "Leggett Street";
+select * from bank_accounts
+    inner join bank_accounts.person_id = people.id
+    
