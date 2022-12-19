ecf05f2ce6be2d72f66f68fe02ba435ce1518634
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-19 09:01:50 +0000

    Mon, Dec 19, 2022, 9:01 AM +00:00

--- fiftyville/small.sql
+++ fiftyville/small.sql
@@ -1,4 +1,4 @@
 select * from passengers
     inner join passengers on passengers.passport_number = people.passport_number
-    inner join 
+    inner join flights on flights.id = passengers.flight_id
     where flights.flight_id = 36;
