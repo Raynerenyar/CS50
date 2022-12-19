7615928d3a4b96869b973dda2ff45caa12bb0a1e
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-19 08:13:23 +0000

    Mon, Dec 19, 2022, 8:13 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -40,3 +40,17 @@ select *
     and month = 7
     and year = 2021
     and hour = 10;
+
+-- linking license plate from security footage
+-- and account numbers that withdrew from atm at Leggett Street at hour 10
+-- reveals a few names
+select * from people
+    inner join bank_accounts on bank_accounts.person_id = people.id
+    inner join atm_transactions on bank_accounts.account_number = atm_transactions.account_number
+    inner join bakery_security_logs on bakery_security_logs.license_plate = people.license_plate
+    where atm_transactions.year = 2021
+    and atm_transactions.month = 7
+    and atm_transactions.day = 28
+    and bakery_security_logs.hour = 10
+    and atm_transactions.transaction_type = "withdraw"
+    and atm_transactions.atm_location = "Leggett Street";
