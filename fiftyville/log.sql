63e1694d0dcebde1fbe942998f60a8b5e42af680
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-19 08:20:25 +0000

    Mon, Dec 19, 2022, 8:20 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -54,3 +54,19 @@ select * from people
     and bakery_security_logs.hour = 10
     and atm_transactions.transaction_type = "withdraw"
     and atm_transactions.atm_location = "Leggett Street";
+
+-- call logs on 28/8/2021 duration less than 60 seconds
+select * from phone_calls where year = 2021 and day = 28 and month = 7 and duration < 60;
+
+select * from people
+    inner join bank_accounts on bank_accounts.person_id = people.id
+    inner join atm_transactions on bank_accounts.account_number = atm_transactions.account_number
+    inner join bakery_security_logs on bakery_security_logs.license_plate = people.license_plate
+    inner join phone_calls on people.phone_number = phone_calls.caller
+    where atm_transactions.year = 2021
+    and atm_transactions.month = 7
+    and atm_transactions.day = 28
+    and bakery_security_logs.hour = 10
+    and atm_transactions.transaction_type = "withdraw"
+    and atm_transactions.atm_location = "Leggett Street"
+    and phone_calls.duration < 60;
