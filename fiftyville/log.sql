d68f4324bd487e420c6080201db706f4ca511992
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-19 05:53:58 +0000

    Mon, Dec 19, 2022, 5:53 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+-- Ruth - thief got into car at bakery parking lot. Security footage might see that.
 select * from interviews where transcript like "%thief%";
