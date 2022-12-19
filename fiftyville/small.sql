cd51672bd9e975a8a06f5d77f3c50e76c6ecfc83
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-19 07:54:54 +0000

    Mon, Dec 19, 2022, 7:54 AM +00:00

--- fiftyville/small.sql
+++ fiftyville/small.sql
@@ -1,6 +1,6 @@
 select * from people
-    inner join bank_accounts.person_id = people.id
     inner join bank_accounts.account_number = transactions.account_number
+    inner join bank_accounts.person_id = people.id
     where year = 2021
     and month = 7
     and day = 28
