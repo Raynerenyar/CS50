c5f1bea10e8d25bde9a43e8e6603b32ac7ab2f45
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-19 06:04:29 +0000

    Mon, Dec 19, 2022, 6:04 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 -- interview about thief, 3 accounts on 28/7/2021
 select * from interviews where transcript like "%thief%";
+-- shows license plate of cars on the date
 select * from bakery_security_logs where day = 28 and month = 7 and year = 2021;
+
+select * from atm_transactions where day = 28 and month = 7 and year = 2021 and atm_location = "Leggett Street"
