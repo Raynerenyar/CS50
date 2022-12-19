e8c73f9803ba97456c4a71c327ab16e8982b33b7
Yong Kim Wui <105347568+Raynerenyar@users.noreply.github.com>
2022-12-19 06:22:38 +0000

    Mon, Dec 19, 2022, 6:22 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,29 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+
 -- interview about thief, 3 accounts on 28/7/2021
-select * from interviews where transcript like "%thief%";
+select *
+    from interviews
+    where transcript like "%thief%";
+
 -- shows license plate of cars on the date. not filtered by activity yet. Thief exited
-select * from bakery_security_logs where day = 28 and month = 7 and year = 2021;
+select *
+    from bakery_security_logs
+    where day = 28
+    and month = 7
+    and year = 2021;
+
 -- shows account numbers that transacted on atm at Leggett Street on the date. Thief withdrew
-select * from atm_transactions where day = 28 and month = 7 and year = 2021 and atm_location = "Leggett Street"
+select *
+    from atm_transactions
+    where day = 28
+    and month = 7
+    and year = 2021
+    and atm_location = "Leggett Street"
+
+-- crime report on the date at Humphrey Street Bakery
+select *
+    from crime_scene_reports
+    where year = 2021
+    and month = 7
+    and day =28
+    and description like "%cs50%"
